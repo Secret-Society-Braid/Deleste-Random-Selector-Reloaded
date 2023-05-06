ebde47a2c00eb4021ce32a79d0b819a33d3949b9
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-05-06 17:50:53 +0900

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module braid.society.secret.drsreloaded {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens braid.society.secret.drsreloaded to javafx.fxml;
+    exports braid.society.secret.drsreloaded;
+}
