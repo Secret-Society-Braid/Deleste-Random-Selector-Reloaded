c4cd3369fad1f87a32b764a7f41dbf392a3df895
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-05-10 18:45:50 +0900

    add records to store each song data

---
+++ src/main/java/braid/society/secret/drsreloaded/internal/database/Song.java
@@ -0,0 +1,3 @@
+package braid.society.secret.drsreloaded.internal.database;
+
+public record Song(String name, boolean doesMasterPlusExist, boolean doesWitchExist, boolean doesGrandExist, boolean doesSmartExist) {}
