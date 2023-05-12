a91ab36b40d778e1d13b9b7a0888070bf19b9dc0
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-05-12 13:02:18 +0900

    Add null check annotation

--- src/main/java/braid/society/secret/drsreloaded/api/database/IListManipulation.java
+++ src/main/java/braid/society/secret/drsreloaded/api/database/IListManipulation.java
@@ -1,10 +1,13 @@
 package braid.society.secret.drsreloaded.api.database;
 
 import braid.society.secret.drsreloaded.internal.database.Song;
+
+import javax.annotation.Nonnull;
 import java.util.List;
 
 public interface IListManipulation {
 
+  @Nonnull
   List<Song> manipulate();
 
   IListManipulation setInGameDifficulty(String difficulty);
