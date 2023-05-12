4610060e0943203e407151e73dbb2eca19ad5e53
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-05-12 12:58:00 +0900

    Add null check annotations to be more convenient coding

--- src/main/java/braid/society/secret/drsreloaded/api/settings/ISettingManager.java
+++ src/main/java/braid/society/secret/drsreloaded/api/settings/ISettingManager.java
@@ -1,9 +1,11 @@
 package braid.society.secret.drsreloaded.api.settings;
 
+import javax.annotation.Nonnull;
 import java.util.List;
 
 public interface ISettingManager {
 
+  @Nonnull
   List<ISettingProp> readSettings();
 
   void writeSettings(List<ISettingProp> settings);
