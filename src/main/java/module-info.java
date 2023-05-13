7661ac550dbe8c83465f2fb39827862661b98684
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-05-13 15:17:43 +0900

    add utils and module expression

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,17 @@
 module braid.society.secret.drsreloaded {
-    requires javafx.controls;
-    requires javafx.fxml;
+  requires javafx.controls;
+  requires javafx.fxml;
 
-    requires org.slf4j;
-    requires lombok;
-    requires jsr305;
-    requires org.jsoup;
-                            
-    opens braid.society.secret.drsreloaded to javafx.fxml;
-    exports braid.society.secret.drsreloaded;
+  requires org.slf4j;
+  requires lombok;
+  requires jsr305;
+  requires org.jsoup;
+  requires com.fasterxml.jackson.databind;
+  requires com.fasterxml.jackson.core;
+  requires com.fasterxml.jackson.annotation;
+
+  opens braid.society.secret.drsreloaded to javafx.fxml;
+  exports braid.society.secret.drsreloaded;
   exports braid.society.secret.drsreloaded.internal.controller;
   opens braid.society.secret.drsreloaded.internal.controller to javafx.fxml;
 }
