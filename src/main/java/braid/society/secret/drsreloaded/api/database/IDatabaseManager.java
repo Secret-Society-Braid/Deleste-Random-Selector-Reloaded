61acac70caaae1735b5925a093a0bf9d234c3bd3
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-05-13 13:43:31 +0900

    Rafctoring classes (#20)
    * refactoring classes that may call as api
    
    * refactoring javafx classes

--- src/main/java/braid/society/secret/drsreloaded/api/database/IDatabaseManager.java
+++ src/main/java/braid/society/secret/drsreloaded/api/database/IDatabaseManager.java
@@ -1,7 +1,5 @@
 package braid.society.secret.drsreloaded.api.database;
 
-import braid.society.secret.drsreloaded.internal.database.Song;
-
 import java.util.List;
 
 public interface IDatabaseManager {
