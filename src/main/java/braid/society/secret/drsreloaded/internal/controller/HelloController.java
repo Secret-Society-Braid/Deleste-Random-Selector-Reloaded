f314645d4dbeba3cd11e281aa2f94ff8d2c599b7
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-05-20 14:54:16 +0900

    add setting ui files

--- src/main/java/braid/society/secret/drsreloaded/internal/controller/HelloController.java
+++ src/main/java/braid/society/secret/drsreloaded/internal/controller/HelloController.java
@@ -1,14 +1,40 @@
 package braid.society.secret.drsreloaded.internal.controller;
 
+import braid.society.secret.drsreloaded.HelloApplication;
+import java.io.IOException;
+import java.util.Objects;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class HelloController {
+
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        try {
+            showSettingWindow();
+        } catch (IOException e) {
+            log.error("Error while showing setting window", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    void showSettingWindow() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("setting.fxml"));
+        Scene settingScene = new Scene(fxmlLoader.load());
+        settingScene.getStylesheets().add(
+            Objects.requireNonNull(HelloApplication.class.getResource("style/setting.css"))
+                .toExternalForm());
+        Stage settingStage = new Stage();
+        settingStage.setScene(settingScene);
+        settingStage.showAndWait();
     }
 }
